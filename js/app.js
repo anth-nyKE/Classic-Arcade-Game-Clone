1d2eac094ec0a3abd5d24e15b6c0f792cbf73f45
anth-nyKE <anthonyjude554@gmail.com>
2018-11-24 10:33:56 +0300

    UPDATED APP>JS
    In this commit, I have added the necessary functions to the app.js file.
    
    The functions allow the sprites and enemies to interact.
    
    The sprite resets upon completion and on a successful cross.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 // Enemies our player must avoid
-var Enemy = function() {
+// Enemy must have a axial position and speed
+const Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -14,6 +18,20 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x+= this.speed * dt; 
+    //refresh the sprite when it crosses
+    if(this.x > 550){
+        this.x = -100;
+        this.speed = 100 + Math.floor(Math.random() * 512)
+    }
+    //collision checker (from https://stackoverflow.com/questions/13916966/adding-collision-detection-to-images-drawn-on-canvas)
+    if (player.x < this.x + 60 &&
+        player.x + 37 > this.x &&
+        player.y < this.y + 25 &&
+        30 + player.y > this.y) {
+        player.x = 200;
+        player.y = 380;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,18 +42,75 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+const Player = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function() {
+    // Contain the sprite to the screen dimensions
+    if (this.y > 380) {
+        this.y = 380;
+    }
+
+    if (this.x > 400) {
+        this.x = 400;
+    }
+
+    if (this.x < 0) {
+        this.x = 0;
+    }
+
+    // Control measure to check for a successful cross
+    if (this.y < 0) {
+        this.x = 200;
+        this.y = 380;
+    }
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(keyPress) {
+    switch (keyPress) {
+        case 'left':
+            this.x -= this.speed + 50;
+            break;
+        case 'up':
+            this.y -= this.speed + 30;
+            break;
+        case 'right':
+            this.x += this.speed + 50;
+            break;
+        case 'down':
+            this.y += this.speed + 30;
+            break;
+    }
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+// The viewport is divided into 3 Y - axis values. Enemies are created in these three Y-axis positions. 
+// The player constructor is called to initialize the player's X&Y axes plus the player's speed
+
+let allEnemies = [], enemyPosition = [60, 140, 220], player = new Player(200, 380, 50), enemy;
+
+enemyPosition.forEach(function(posY) {
+    enemy = new Enemy(0, posY, 100 + Math.floor(Math.random() * 512));
+    allEnemies.push(enemy);
+});
 
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    const allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
